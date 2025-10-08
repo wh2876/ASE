b6fe9d0fc97745b14e72ab82d22c452b73d4bfb2
wh2876 <w.hart2876@student.leedsbeckett.ac.uk>
2025-10-08 09:49:11 +0100

    added ses1 stuff

---
+++ SES1/Session1/Session1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+        }
+
+        public Program()
+        {
+            while (true)
+            {
+                float h = 0;
+                float w = 0;
+                bool f = false;
+
+                while (true)
+                {
+                    try
+                    {
+                        MessageBox.Show("Please enter your height in meters", "Hi_0", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        h = Single.Parse(Console.ReadLine());
+                        if (h > 0) break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Invalid response.");
+                    }
+                } 
+
+                while (true) {
+                    try
+                    {
+                        MessageBox.Show("Please enter your weight in kg", "Hi_1", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        w = Single.Parse(Console.ReadLine());
+                        if( w > 0 ) break;
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine("Invalid response.");
+                    }
+                }
+                while (true)
+                {
+                    MessageBox.Show("Please enter your sex:", "Hi_1", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    String sex = Console.ReadLine().ToLower();
+                    if (sex == "female") break;
+                    else if (sex == "male") break;
+                    else
+                    {
+                        Console.WriteLine("Please only input \"male\" or \"female\"");
+                    }
+                }
+                MessageBox.Show("You are " + BMIGrade(calculateBMI(h, w), f), "Hi_1", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        public float calculateBMI(float height, float weight)
+        {
+            return weight / (height * height);
+        }
+
+        public String BMIGrade(float BMI, bool female)
+        {
+            if (BMI < 17.5 && female) return "severely underweight";
+            else if (BMI < 18.5 && !female) return "severely underweight";
+            else if (BMI <= 19.1 && female) return "underweight";
+            else if (BMI <= 20.7 && !female) return "underweight";
+            else if (BMI <= 25.8 && female) return "in normal range";
+            else if (BMI <= 26.4 && !female) return "in normal range";
+            else if (BMI <= 27.3 && female) return "overweight";
+            else if (BMI <= 27.8 && !female) return "overweight";
+            else if (BMI <= 32.3 && female) return "marginally overweight";
+            else if (BMI <= 31.1 && !female) return "marginally overweight";
+            else if (BMI <= 35) return "very overweight or obese";
+            else if (BMI <= 40) return "severely obese";
+            else if (BMI <= 50) return "morbidly obese";
+            else if (BMI <= 60) return "super obese";
+            else return "mind blowingly obese";
+        }
+
+    }
+}
---
+++ SES1/Session1/Session1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Session1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Leeds Beckett University")]
+[assembly: AssemblyProduct("Session1")]
+[assembly: AssemblyCopyright("Copyright © Leeds Beckett University 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("afd5332a-d144-418c-87e2-b1b6c32f9fe6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
